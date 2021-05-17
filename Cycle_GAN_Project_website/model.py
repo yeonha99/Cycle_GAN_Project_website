7182080255d84428565955ef6181e5b3ec665a03
yeonha99 <60427107+yeonha99@users.noreply.github.com>
2021-05-17 12:11:25 +0900

    Update model.py
    안쓰는거 제거

--- Cycle_GAN_Project_website/model.py
+++ Cycle_GAN_Project_website/model.py
@@ -4,122 +4,54 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+from layer import *
 
-class DECBR2d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1, bias=True, norm="bnorm", relu=0.0):
-        super().__init__()
+## 네트워크 구축하기
+# CycleGAN
+# https://arxiv.org/pdf/1703.10593.pdf
 
-        layers = []
-        # layers += [nn.ReflectionPad2d(padding=padding)]
-        layers += [nn.ConvTranspose2d(in_channels=in_channels, out_channels=out_channels,
-                                      kernel_size=kernel_size, stride=stride, padding=padding, output_padding=output_padding,
-                                      bias=bias)]
+class CycleGAN(nn.Module):
+    def __init__(self, in_channels, out_channels, nker=64, norm='bnorm', nblk=6):
+        super(CycleGAN, self).__init__()
 
-        if not norm is None:
-            if norm == "bnorm":
-                layers += [nn.BatchNorm2d(num_features=out_channels)]
-            elif norm == "inorm":
-                layers += [nn.InstanceNorm2d(num_features=out_channels)]
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.nker = nker
+        self.norm = norm
+        self.nblk = nblk
 
-        if not relu is None and relu >= 0.0:
-            layers += [nn.ReLU() if relu == 0 else nn.LeakyReLU(relu)]
+        if norm == 'bnorm':
+            self.bias = False
+        else:
+            self.bias = True
 
-        self.cbr = nn.Sequential(*layers)
+        self.enc1 = CBR2d(self.in_channels, 1 * self.nker, kernel_size=7, stride=1, padding=3, norm=self.norm, relu=0.0)
+        self.enc2 = CBR2d(1 * self.nker, 2 * self.nker, kernel_size=3, stride=2, padding=1, norm=self.norm, relu=0.0)
+        self.enc3 = CBR2d(2 * self.nker, 4 * self.nker, kernel_size=3, stride=2, padding=1, norm=self.norm, relu=0.0)
 
-    def forward(self, x):
-        return self.cbr(x)
+        if self.nblk:
+            res = []
 
+            for i in range(self.nblk):
+                res += [ResBlock(4 * self.nker, 4 * self.nker, kernel_size=3, stride=1, padding=1, norm=self.norm, relu=0.0)]
 
-class CBR2d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, padding_mode='reflection', bias=True, norm="bnorm", relu=0.0):
-        super().__init__()
+            self.res = nn.Sequential(*res)
 
-        layers = []
-
-        if padding_mode == 'reflection':
-            layers += [nn.ReflectionPad2d(padding)]
-        elif padding_mode == 'replication':
-            layers += [nn.ReplicationPad2d(padding)]
-        elif padding_mode == 'constant':
-            value = 0
-            layers += [nn.ConstantPad2d(padding, value)]
-        elif padding_mode == 'zeros':
-            layers += [nn.ZeroPad2d(padding)]
-
-        layers += [nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
-                             kernel_size=kernel_size, stride=stride, padding=0,
-                             bias=bias)]
-
-        if not norm is None:
-            if norm == "bnorm":
-                layers += [nn.BatchNorm2d(num_features=out_channels)]
-            elif norm == "inorm":
-                layers += [nn.InstanceNorm2d(num_features=out_channels)]
-
-        if not relu is None and relu >= 0.0:
-            layers += [nn.ReLU() if relu == 0 else nn.LeakyReLU(relu)]
-
-        self.cbr = nn.Sequential(*layers)
+        self.dec3 = DECBR2d(4 * self.nker, 2 * self.nker, kernel_size=3, stride=2, padding=1, norm=self.norm, relu=0.0)
+        self.dec2 = DECBR2d(2 * self.nker, 1 * self.nker, kernel_size=3, stride=2, padding=1, norm=self.norm, relu=0.0)
+        self.dec1 = CBR2d(1 * self.nker, self.out_channels, kernel_size=7, stride=1, padding=3, norm=None, relu=None)
 
     def forward(self, x):
-        return self.cbr(x)
+        x = self.enc1(x)
+        x = self.enc2(x)
+        x = self.enc3(x)
 
+        x = self.res(x)
 
-class ResBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True, norm="bnorm", relu=0.0):
-        super().__init__()
+        x = self.dec3(x)
+        x = self.dec2(x)
+        x = self.dec1(x)
 
-        layers = []
-
-        # 1st conv
-        layers += [CBR2d(in_channels=in_channels, out_channels=out_channels,
-                         kernel_size=kernel_size, stride=stride, padding=padding,
-                         bias=bias, norm=norm, relu=relu)]
-
-        # 2nd conv
-        layers += [CBR2d(in_channels=out_channels, out_channels=out_channels,
-                         kernel_size=kernel_size, stride=stride, padding=padding,
-                         bias=bias, norm=norm, relu=None)]
-
-        self.resblk = nn.Sequential(*layers)
-
-    def forward(self, x):
-        return x + self.resblk(x)
-
-
-class PixelUnshuffle(nn.Module):
-    def __init__(self, ry=2, rx=2):
-        super().__init__()
-        self.ry = ry
-        self.rx = rx
-
-    def forward(self, x):
-        ry = self.ry
-        rx = self.rx
-
-        [B, C, H, W] = list(x.shape)
-
-        x = x.reshape(B, C, H // ry, ry, W // rx, rx)
-        x = x.permute(0, 1, 3, 5, 2, 4)
-        x = x.reshape(B, C * (ry * rx), H // ry, W // rx)
-
-        return x
-
-
-class PixelShuffle(nn.Module):
-    def __init__(self, ry=2, rx=2):
-        super().__init__()
-        self.ry = ry
-        self.rx = rx
-
-    def forward(self, x):
-        ry = self.ry
-        rx = self.rx
-
-        [B, C, H, W] = list(x.shape)
-
-        x = x.reshape(B, C // (ry * rx), ry, rx, H, W)
-        x = x.permute(0, 1, 4, 2, 5, 3)
-        x = x.reshape(B, C // (ry * rx), H * ry, W * rx)
+        x = torch.tanh(x)
 
         return x
